531b10d94f29dc67704c42d71c2a1d23783d1c46
Alberto <albertovalleflores99@gmail.com>
2021-11-16 12:01:30 -0800

    Add append list/set

--- flaskapp/static/js/workout.js
+++ flaskapp/static/js/workout.js
@@ -5,4 +5,10 @@ $( document ).ready( () => {
     $(".close-button").click( () => {
         $('.workout-tab').css('visibility',"hidden");
     })
+    $("#add-set").click( () => {
+        var content_table = document.getElementById("workout-tab-content");
+        var add_exercise_eleme = document.getElementById("add-exercise");
+        var cloned_elem = add_exercise_eleme.cloneNode(true);
+        content_table.appendChild(cloned_elem);
+    })
 });
