51ce3fbec18a56cd0e9bc9959bdbeb8e264d7679
Alberto <albertovalleflores99@gmail.com>
2021-11-17 20:05:28 -0800

    Add and fixed queries

--- queries.sql
+++ queries.sql
@@ -7,18 +7,24 @@ INSERT INTO trainer (t_userID)
     WHERE u_name = ? AND
           u_password = ?;
 
---3 Selects a user from the user table with the given u_name and u_password (login)
+--3: Update trainer bit in user (adding trainers)
+UPDATE user
+Set u_trainer = 1
+where u_name = ?
+and u_password = ?
+
+--4 Selects a user from the user table with the given u_name and u_password (login)
 SELECT * FROM user 
 WHERE u_name = ? AND
       u_password = ?;
 
---4: Sets a user as a customer by putting their u_userID into the customer table
+--5: Sets a user as a customer by putting their u_userID into the customer table
 INSERT INTO customer (c_userID)
     SELECT u_userID FROM user
     WHERE u_name = ? AND
           u_password = ?;
 
---5: Search exercise by category
+--6: Search exercise by category
 SELECT e_name
   FROM category,
        exercise,
@@ -27,13 +33,13 @@ SELECT e_name
        ec_exerciseID = e_exerciseID AND
        c_name = ?;
 
---6: Search by exercise name
-SELECT e_name
+--7: Search exercise ID by exercise name
+SELECT e_exerciseID
   FROM exercise
  WHERE e_name = ?;
 
 
---7: Populating category table:
+--8: Populating category table:
 INSERT into category (c_name)
     VALUES
     ('legs'),
@@ -52,34 +58,36 @@ INSERT into category (c_name)
     ('lower back'),
     ('upper back');
 
---8 selects all exercises from exercise table so user 
---  can select an exercise from the list of exercises
+--9 Select all exercises names from exercise table in alphabetical order
+-- (user needs to see all possible exercises to select from when adding workout)
 SELECT e_name
   FROM exercise
  ORDER BY e_name;
 
 
 -----------------------QUERIES FOR ADDING A WORKOUT----------------------
---9 First we create a new training session which contains all workouts performed during that session
-INSERT INTO training_session (r_userID, r_datecompleted) VALUES (?, ?);
+--10 First we create a new training session which contains all workouts performed during that session
+INSERT INTO training_session (r_userID, r_datecompleted)
+SELECT u_userID, ? FROM user
+    WHERE u_name = ?;
 
---10 Then we create the set based on USERs s_reps, s_weight, and s_duration
+--11 Then we create the set based on USERs s_reps, s_weight, and s_duration
 INSERT INTO sets (s_reps, s_weight, s_duration) VALUES (?, ?, ?);
 
---11 Add a workout
-INSERT into workout (w_sessionID, w_excerciseID, w_setID)
-SELECT r_sessionID, e_exerciseID, s_setID
-from 
-(SELECT r_sessionID from training_session where r_userID = ? and r_datecompleted = ?), 
-(select e_exerciseID from exercise where e_name = ?), 
-(select s_setID from sets where s_reps = ? and s_weight = ? and s_duration = ?)
+--12 Add a workout
+INSERT into workout (w_sessionID, w_exerciseID, w_setID)
+SELECT r_sessionID, e_exerciseID, ?
+from
+(SELECT r_sessionID from training_session, user where r_userID = u_userID AND u_name = ? and r_datecompleted = ?),
+(select e_exerciseID from exercise where e_name = ?);
 
 
---12 Select the name, exercise, sets, reps, and weight
+--13 Show all workouts completed by a user
 SELECT e_name,
        s_setID,
        s_reps,
-       s_weight
+       s_weight,
+       r_datecompleted
   FROM user,
        training_session,
        workout,
@@ -87,13 +95,15 @@ SELECT e_name,
        exercise
  WHERE u_userID = r_userID AND
        w_setID = s_setID AND
-       w_exerciseID = e_exerciseID and
+       w_sessionID = r_sessionID AND
+       w_exerciseID = e_exerciseID AND
        u_name = ?;
 
 
---13 Calculate progress by selecting average weight between date range for a
+--14 Calculate progress by selecting average weight between date range for a *****************************
 -- specific user and exercise
-SELECT AVG(s_weight)
+SELECT AVG(s_weight),
+       r_datecompleted
   FROM user,
        training_session,
        workout,
@@ -105,9 +115,10 @@ SELECT AVG(s_weight)
        w_exerciseID = e_exerciseID AND
        u_name = ? AND
        e_name = ? AND
-       r_datecompleted BETWEEN ? AND ?;
+       r_datecompleted BETWEEN ? AND ?
+       group by r_datecompleted;
 
---14 Returns all exercises done on a specific date
+--15 Returns all exercises done on a specific date
 SELECT r_datecompleted,
        e_name,
        s_reps,
@@ -121,13 +132,12 @@ SELECT r_datecompleted,
        w_exerciseID = e_exerciseID AND
        w_setID = s_setID AND
        u_userID = r_userID AND
-       r_datecompleted = ? AND
-       u_name = ?
+       u_name = ? AND
+       r_datecompleted = ?
  GROUP BY s_setID
- ORDER BY e_name;
+ ORDER BY e_name, s_weight ASC;
 
-
---15 Search for workouts by exercise
+--16 Search for workouts details by exercise name
 SELECT r_datecompleted,
        e_name,
        s_reps,
@@ -147,32 +157,58 @@ SELECT r_datecompleted,
  ORDER BY r_datecompleted;
 
 ----------------SUBSCRIPTIONS---------------
---16 Add new subscription
-INSERT INTO subscription (su_customerID, su_trainerID) VALUES (?, ?);
+--17 Add new subscription (trainer name taken from UI and customer from flask session)
+INSERT INTO subscription (su_trainerID, su_customerID)
+select t_trainerID, c_customerID from
+(Select t_trainerID from user, trainer where t_userID = u_userID and u_name = ?),
+(select c_customerID from user, customer where u_userID = c_userID and u_name = ?);
 
---17 Select the user id from customer table
-SELECT c_customerID
-FROM user, customer
-WHERE u_userID = c_userID AND
-      u_name = ?;
+
+--18 Select all workouts from a trainer the user is subscribed to on a given date.**********************************
+SELECT r_datecompleted,
+       e_name,
+       s_reps,
+       s_weight
+FROM user u1,
+user u2,
+customer,
+trainer,
+subscription,
+training_session,
+workout,
+exercise,
+sets
+where
+u1.u_userID = c_userID AND
+c_customerID = su_customerID AND
+t_trainerID = su_trainerID AND
+u2.u_name = ? AND --trainer name
+u1.u_name = ? AND --user name
+r_userID = u2.u_userID AND
+w_exerciseID = e_exerciseID AND
+r_sessionID = w_sessionID AND
+s_setID = w_setID AND
+r_datecompleted = ?;
+
 
 -----------------BODY---------------------
---18 Insert body foir each created user
-INSERT INTO body (b_age, b_gender, b_height, b_weight)
-    SELECT u_userID, ?,?,? FROM user
-    WHERE u_name = ?
-    AND u_password = ?;
+--19 Insert body for each created user
+INSERT INTO body (b_userID, b_age, b_gender, b_height, b_weight)
+Select u_userID, ?,?,?,? from
+(SELECT u_userID from user
+    where u_name = ?
+    and u_password = ?)
 
 
---19 Edit Body (would be same for height, weight, age)
+--20 Edit Body (would be same for height, weight, age)
 UPDATE body
-   SET b_height = ?
+   SET b_weight = ?
   FROM user
  WHERE b_userID = u_userID AND
        u_name = ?;
 
 
---20 Show Body
+--21 Show Body
 SELECT b_age,
        b_gender,
        b_height,
@@ -182,7 +218,27 @@ SELECT b_age,
  WHERE u_userID = b_userID AND
        u_name = ?;
 
---21 Show number of repetitions of all exercises
+
+--22 Unsubscribe customer from trainer
+DELETE FROM subscription
+      WHERE su_trainerID IN (
+                SELECT t_trainerID
+                  FROM user,
+                       trainer
+                 WHERE u_userID = t_userID
+                 and u_name = ?
+            )
+AND
+            su_customerID IN (
+    SELECT c_customerID
+      FROM user,
+           customer
+     WHERE u_userID = c_userID
+     and u_name = ?
+);
+
+
+--23 Show number of repetitions of all exercises
 SELECT SUM(s_reps)
   FROM user,
        training_session,
